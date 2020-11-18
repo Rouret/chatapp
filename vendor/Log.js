ca2bb4cb74e4f55b2eeb1294266112ea8e6897ac
Rouret Lucas <lucas.rouretperso@gmail.com>
2020-11-18 18:01:47 +0100

    Add basic log feature"

---
+++ vendor/Log.js
@@ -0,0 +1,21 @@
+class Log {
+    static INFO = "INFO"
+    static WARNING = "WARNING"
+    static ERROR = "ERROR"
+
+    static display(message, ERROR_TYPE) {
+        console.log(`${Log.getTextDateAndTime()} ${ERROR_TYPE} : ${message}`);
+    }
+
+    static getTextDateAndTime() {
+        const currentdate = new Date();
+        return "[" + currentdate.getDate() + "/" +
+            (currentdate.getMonth() + 1) + "/" +
+            currentdate.getFullYear() + " " +
+            currentdate.getHours() + ":" +
+            currentdate.getMinutes() + ":" +
+            currentdate.getSeconds() + "]";
+    }
+}
+
+module.exports = Log
